8247fc5aa28b005cbb4499bf1e541e3b2b7c0253
Ali Sharafzade <ali.sharafzade1991@gmail.com>
2019-03-04 20:06:16 +0330

    حذف ويو index اصلي

--- ClassProject/ClassProject/Controllers/HomeController.cs
+++ ClassProject/ClassProject/Controllers/HomeController.cs
@@ -13,5 +13,9 @@ namespace ClassProject.Controllers
         {
             return View();
         }
+        public ActionResult OurMenus()
+        {
+            return View();
+        }
     }
 }
